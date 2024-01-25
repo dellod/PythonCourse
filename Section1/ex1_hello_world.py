a9c1ced2eb62339931f1ef2b998f20f9fec90c7c
dellod <daryl.dang4@gmail.com>
2024-01-25 01:37:23 -0500

    [Section 1] Add example 1

---
+++ Section1/ex1_hello_world.py
@@ -0,0 +1,9 @@
+# ex1_hello_world.py
+# Udemy Python Course
+# Daryl
+
+# Description: This is TYPICALLY a programmers first programming problem! It teaches how to use the
+#              print statement and read that from the console.
+
+print("H3ll0 @ World!!!!") # Note: you can have a combination of any characters as long as it is
+                           #       surrounded by ""
